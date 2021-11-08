18c9d9908a2eae0f6d412b63135333b4722802ee
iraVlasenko <irina.mala.v@gmail.com>
2021-11-08 18:11:42 +0200

    3

--- 07.11/js/main.js
+++ 07.11/js/main.js
@@ -1,4 +1,27 @@
-/*Задачка, перебрать массив и изменить в нем данные, результат вывести в консольку
+
+
+
+/*
+
+
+git status
+
+git add .
+
+>git commit -m "2"
+
+git push
+
+Enumerating objects: 3, done.
+Counting objects: 100% (3/3), done.
+Delta compression using up to 4 threads
+Compressing objects: 100% (1/1), done.
+Writing objects: 100% (2/2), 223 bytes | 223.00 KiB/s, done.
+Total 2 (delta 0), reused 0 (delta 0), pack-reused 0
+To https://github.com/iraVlasenko/1.git
+   696d6ff..3844e37  main -> main
+
+D:\Users\student\Desktop\front 10\08.11.21\1>Задачка, перебрать массив и изменить в нем данные, результат вывести в консольку
 Исходный массив:
 [123, 'voda', ['q', 'w', 'e' ], { a: 'b', v: 'qwe', c: 123} ]
 Новый массив
